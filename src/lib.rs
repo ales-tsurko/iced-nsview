00ef0b0bf407bb9a5ae8f5fcddaaafa2afd2cb82
Ales Tsurko <ales.tsurko@gmail.com>
2020-06-11 01:31:55 +0300

    Add IcedView

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 //! This crate allows you to use Iced as NSView. Thus it makes Iced embeddable into macOS
 //! application or AU/VST plugins, for example.
+//!
+//! You should implement your GUI using `program::Program`, then you can init `IcedView` from it.
 
 #![deny(
     nonstandard_style,
@@ -16,3 +18,37 @@
     unused_qualifications,
     unreachable_pub
 )]
+
+use std::ffi::c_void;
+
+use objc::class;
+use objc::declare::ClassDecl;
+
+pub use iced_native::*;
+
+/// Iced view subclassed from NSView.
+pub struct IcedView<P: program::Program> {
+    // raw_ptr: *mut c_void,
+    decl: ClassDecl,
+    program: P,
+}
+
+impl<P: program::Program> IcedView<P> {
+    /// Constructor.
+    pub fn new(program: P) -> Self {
+        let superclass = class!(NSView);
+        let decl = ClassDecl::new("IcedView", superclass).expect("Can't allocate IcedView");
+
+        Self { decl, program }
+    }
+
+    /// Get a raw pointer to the Cocoa view.
+    pub fn raw_ptr(&self) -> *mut c_void {
+        todo!()
+    }
+
+    /// Make this view a subview of another view.
+    pub fn make_subview_of(&self, view: *mut c_void) {
+        todo!()
+    }
+}
