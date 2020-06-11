53b8049574efbdc83136ea25804fe57fcacd7cdd
Ales Tsurko <ales.tsurko@gmail.com>
2020-06-12 00:09:50 +0300

    Init window in the slider example

--- examples/slider.rs
+++ examples/slider.rs
@@ -1,12 +1,45 @@
+use cocoa::appkit::NSView;
+
 use iced_nsview::{
-    slider, Align, Color, Column, Command, Element, IcedView, Length, Row, Size, Slider,
-    Text, Viewport, Application,
+    slider, Align, Application, Color, Column, Command, Element, IcedView, Length, Row, Size,
+    Slider, Text, Viewport,
 };
 
+use winit::dpi;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::platform::macos::WindowExtMacOS;
+use winit::window::WindowBuilder;
+
 fn main() {
+    let size = Size::new(800, 600);
+    let win_size = dpi::Size::Physical((size.width, size.height).into());
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_inner_size(win_size)
+        .build(&event_loop)
+        .unwrap();
+
     let controls = Controls::new();
-    let viewport = Viewport::with_physical_size(Size::new(800, 600), 1.0);
+    let viewport = Viewport::with_physical_size(size, window.scale_factor());
     let view = IcedView::new(controls, viewport);
+    unsafe { view.make_subview_of(window.ns_view()) };
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Poll;
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            Event::MainEventsCleared => {
+                window.request_redraw();
+            }
+            Event::RedrawRequested(_) => {}
+            _ => (),
+        }
+    });
 }
 
 struct Controls {
