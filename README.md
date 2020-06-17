37fe71a61917d66284673a588607133c8a8e6c82
Ales Tsurko <ales.tsurko@gmail.com>
2020-06-17 20:06:35 +0300

    Update README

--- README.md
+++ README.md
@@ -3,5 +3,10 @@
 This crate allows you to use Iced as NSView. Thus it makes Iced embeddable into
 a macOS application or AU/VST plugins, for example.
 
+
+
+
+## Usage
+
 You should implement your GUI using `Application` trait, then you can initialize
 `IcedView` with it.
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 //! This crate allows you to use Iced as NSView. Thus it makes Iced embeddable into a macOS
 //! application or AU/VST plugins, for example.
+//! 
+//! ## Usage
 //!
 //! You should implement your GUI using `Application` trait, then you can initialize `IcedView`
 //! with it.
